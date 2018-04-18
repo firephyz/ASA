0c359d3a54e50a08689737d01463cb1626dac751
Kyle Burge <kyle.burge7196@gmail.com>
2018-04-18 17:44:46 -0400

    Updated waveform to be more accurate.

--- fes_signal/fes_signal.ino
+++ fes_signal/fes_signal.ino
@@ -10,11 +10,13 @@
 uint16_t waveform[NUM_OF_SAMPLES];
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(2, OUTPUT);
   analogWriteResolution(12);
 
-  double e_coef_1 = pow(1 / 0.333, (double)RAMP_SAMPLES / (NUM_OF_SAMPLES - RAMP_SAMPLES));
-  double e_coef_2 = log(1 / 0.333) / (NUM_OF_SAMPLES - RAMP_SAMPLES);
+//  double e_coef_1 = pow(1 / 0.333, (double)RAMP_SAMPLES / (NUM_OF_SAMPLES - RAMP_SAMPLES));
+//  double e_coef_2 = log(1 / 0.333) / (NUM_OF_SAMPLES - RAMP_SAMPLES);
+  double e_coef_1 = 0.66666;
+  double e_coef_2 = 0.2;
 
   uint16_t value = 0;
   for(int i = 0; i < NUM_OF_SAMPLES; ++i) {
@@ -22,7 +24,7 @@ void setup() {
       value = DTV(i * (1.0 / (RAMP_SAMPLES - 1)));
     }
     else {
-      value = DTV(e_coef_1 * exp(-i * e_coef_2));
+      value = DTV(e_coef_1 * exp(-(i-6) * e_coef_2)) + DTV(1.0 / 3);
     }
 
     waveform[i] = value;
